8dae630c9bd075af61c0c25c9c173765398e2632
Pirate5946 <739686950@qq.com>
2020-02-13 16:35:57 +0800

    更新hashMap整理

---
+++ doc/TODO.md
@@ -0,0 +1,46 @@
+待整理面经
+- https://mp.weixin.qq.com/s?__biz=Mzg2OTA0Njk0OA==&mid=2247485167&idx=1&sn=a35fad235a6bb2b6e31f1ff37e72bfd7&chksm=cea24924f9d5c032cbbeffc87cd366e9aa7b209897a26ad5c7f7b1b766b3c34a2c9b6870006c&mpshare=1&scene=1&srcid=#rd
+
+### 2月
+#### 2.10 - 2.14 、 2.15 - 2.21
+集合知识点、问题与答案
+- list
+- set
+- hashMap、
+- concurrentHashMap
+
+#### 2.15 - 2.21
+java基础
+
+多线程 ： 
+- 云笔记资料，
+- 整理问题与答案，
+- 手写代码
+
+#### 2.22 - 28
+mysql
+- [MySQL的万字总结（缓存，索引，Explain，事务，redo日志等）](https://juejin.im/post/5dfc846051882512327a63b6)
+- 索引结构 B+树
+- 手写sql练习
+
+算法
+- 堆排序、topK
+- 快速排序
+- https://leetcode-cn.com/problems/reverse-words-in-a-string/
+- https://leetcode-cn.com/problems/lexicographical-numbers/
+
+
+
+
+### 3月
+#### 2.29 - 3.6
+项目介绍、亮点难点实现
+- 整理项目代码（具体到代码，关键思路，关键类）
+    - redis的使用
+        - 常用接口
+        - 分布式锁、获取订单号
+    - rabbit MQ的使用
+    - shiro的使用
+    - spring AOP的使用
+    - 并发接口限流 
+    
--- src/main/java/base/Test.java
+++ src/main/java/base/Test.java
@@ -4,6 +4,7 @@ import cn.hutool.cache.CacheUtil;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * @ClassName Test
@@ -14,11 +15,25 @@ import java.util.LinkedList;
  **/
 public class Test {
     public static void main(String[] args) {
-        String pnr = "R1RT HE0RTQW";
-        System.out.println(pnr.indexOf("RT"));
+//        String pnr = "R1RT HE0RTQW";
+//        System.out.println(pnr.indexOf("RT"));
 
 //        CacheUtil
 //        HashMap
 
+        // 测试 put 方法
+//        HashMap<String, Integer> hashMap = new HashMap<>(1);
+//        hashMap.put("1", 1);
+//        Integer put = hashMap.put("1", 11);
+//        hashMap.put("2", 2);
+//        hashMap.put("3", 3);
+//        System.out.println(put);
+//        System.out.println(hashMap.toString());
+
+        // 测试 get 方法
+        Map<String, Integer> map = new HashMap<String, Integer>(1);
+        Integer put = map.put("1", 1);
+        Integer integer = map.get("1");
+        System.out.println(integer);
     }
 }
