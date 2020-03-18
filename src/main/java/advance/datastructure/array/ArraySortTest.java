eba0cf2e47e7e3f54e47dfdeccf32098520fbccc
Pirate5946 <739686950@qq.com>
2020-03-18 19:10:29 +0800

    ...

--- src/main/java/advance/datastructure/array/ArraySortTest.java
+++ src/main/java/advance/datastructure/array/ArraySortTest.java
@@ -33,8 +33,8 @@ public class ArraySortTest {
 //                    return -1;
 //                }
 //                return 0;
-//                return o2.compareTo(o1);
-                return o1.compareTo(o2);
+                return o2.compareTo(o1);
+//                return o1.compareTo(o2);
             }
 
         });
--- src/main/java/base/Test.java
+++ src/main/java/base/Test.java
@@ -1,10 +1,13 @@
 package base;
 
+import advance.datastructure.User;
 import cn.hutool.cache.CacheUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @ClassName Test
@@ -31,9 +34,20 @@ public class Test {
 //        System.out.println(hashMap.toString());
 
         // 测试 get 方法
-        Map<String, Integer> map = new HashMap<String, Integer>(1);
-        Integer put = map.put("1", 1);
-        Integer integer = map.get("1");
-        System.out.println(integer);
+//        Map<String, Integer> map = new HashMap<String, Integer>(1);
+//        Integer put = map.put("1", 1);
+//        Integer integer = map.get("1");
+//        System.out.println(integer);
+
+        List<User> users = new ArrayList<>(2);
+        User user1 = new User(12);
+        User user2 = new User(13);
+        users.add(user1);
+        users.add(user2);
+        String jsonStr = JSONUtil.toJsonStr(users);
+        System.out.println(jsonStr);
+        JSONArray jsonArray = JSONUtil.parseArray(jsonStr);
+        List<User> userList = JSONUtil.toList(jsonArray, User.class);
+        System.out.println(JSONUtil.toJsonStr(userList));
     }
 }
